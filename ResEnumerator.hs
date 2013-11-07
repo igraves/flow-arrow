7de3a9491099a97f8e22b62fa752e24d3ca3eb2f
Ian Graves <thegravian@gmail.com>
2013-11-07 17:56:10 -0600

    Instance of Category

--- ResEnumerator.hs
+++ ResEnumerator.hs
@@ -1,6 +1,18 @@
+{-#LANGUAGE RankNTypes #-}
+
 module REnumerator where
 
 import Control.Monad.Resumption.Reactive
+import Control.Category
+import Prelude hiding ((.),id)
+
+
+newtype Stream m a b = Stream { deStream :: forall c. a -> ReacT a b m c}
+
+instance Monad m => Category (Stream m) where
+  id = Stream $ preact id 
+  a . b = (flip (<//>)) a b 
+
 
 (/>) :: (Monad m) => ReacT a b m c -> ReacT b d m c -> (a -> ReacT a d m c)
 a /> b = \input -> ReacT $ do
@@ -16,16 +28,31 @@ a /> b = \input -> ReacT $ do
                                                                       (rresult,rcmp) <- runTick (cr) lresult
                                                                       return $ Right (rresult,lcmp `knot` rcmp)
       where
-          fromRight (Right r) = r
           runTick (_, f) inp = do 
                                  r <- deReacT $ f inp
                                  return $ fromRight r
-          knot lre rre = \input -> ReacT $ do
-                                            (loutput,lres) <- (deReacT $ lre input) >>= return . fromRight
-                                            (routput,rres) <- (deReacT $ rre loutput) >>= return . fromRight
-                                            return $ Right (routput,knot lres rres) 
-                            
 
+(<//>) :: Monad m => Stream m a b -> Stream m b c -> Stream m a c
+a <//> b = Stream $ (deStream a) `knot` (deStream b) 
+
+knot :: Monad m => (input -> ReacT input output1 m a) -> 
+                   (output1 -> ReacT output1 output m a) -> 
+                   (input -> ReacT input output m a)
+
+knot lre rre = \input -> ReacT $ do
+                                   lre' <- deReacT $ lre input
+                                   case lre' of
+                                          Left (res) -> return $ Left res 
+                                          Right (loutput,lres) -> do
+                                                                    rre' <- deReacT $ rre loutput
+                                                                    case rre' of
+                                                                           Left (res) -> return $  Left res
+                                                                           Right (routput,rres) -> return $ Right (routput, lres `knot` rres) 
+                            
+fromRight (Right r) = r
+
+preact :: Monad m => (input -> output) -> (input -> ReacT input output m term)
+preact f = \input -> ReacT $ return $ Right (f input, preact f)
                            
                 
                                 
