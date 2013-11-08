6a5dafbc0dbf719f51a75aec2617af540eb1fd16
Ian Graves <thegravian@gmail.com>
2013-11-07 21:33:57 -0600

    Instance of Arrow.

--- ResEnumerator.hs
+++ ResEnumerator.hs
@@ -4,6 +4,7 @@ module REnumerator where
 
 import Control.Monad.Resumption.Reactive
 import Control.Category
+import Control.Arrow
 import Prelude hiding ((.),id)
 
 
@@ -12,6 +13,21 @@ newtype Stream m a b = Stream { deStream :: forall c. a -> ReacT a b m c}
 instance Monad m => Category (Stream m) where
   id = Stream $ preact id 
   a . b = (flip (<//>)) a b 
+
+instance Monad m => Arrow (Stream m) where
+  arr f = Stream $ preact f 
+  first stream = Stream $ \(input,copy) -> ReacT $ do
+                                                    cmp <- deReacT $ (deStream stream) input
+                                                    case cmp of
+                                                          Left v -> return $ Left v 
+                                                          Right (output,f) -> return $ Right ((output,copy),endrun f)
+        where
+          --endrun :: Monad m => (a -> ReacT a b m c) -> ((a,d) -> ReacT (a,d) (b,d) m c)
+          endrun res = \(input,copy) -> ReacT $ do
+                                                  cmp <- deReacT $ res input
+                                                  case cmp of
+                                                       Left v -> return $ Left v 
+                                                       Right (output,res') -> return $ Right ((output,copy), endrun res')
 
 
 (/>) :: (Monad m) => ReacT a b m c -> ReacT b d m c -> (a -> ReacT a d m c)
@@ -24,17 +40,20 @@ a /> b = \input -> ReacT $ do
                                                 case r of
                                                      Left  term' -> return $ Left term'
                                                      Right cr    -> do
-                                                                      (lresult,lcmp) <- runTick (cl) input
+                                                                      (lresult,lcmp) <- runTick (cl) input --These aren't correct, knot is correct, however.
                                                                       (rresult,rcmp) <- runTick (cr) lresult
                                                                       return $ Right (rresult,lcmp `knot` rcmp)
       where
           runTick (_, f) inp = do 
                                  r <- deReacT $ f inp
                                  return $ fromRight r
+          fromRight (Right r) = r
 
 (<//>) :: Monad m => Stream m a b -> Stream m b c -> Stream m a c
 a <//> b = Stream $ (deStream a) `knot` (deStream b) 
 
+--Knot takes two functions producing ReacT of chainable signals
+--and merges them into one reactive resumption
 knot :: Monad m => (input -> ReacT input output1 m a) -> 
                    (output1 -> ReacT output1 output m a) -> 
                    (input -> ReacT input output m a)
@@ -49,12 +68,6 @@ knot lre rre = \input -> ReacT $ do
                                                                            Left (res) -> return $  Left res
                                                                            Right (routput,rres) -> return $ Right (routput, lres `knot` rres) 
                             
-fromRight (Right r) = r
 
 preact :: Monad m => (input -> output) -> (input -> ReacT input output m term)
 preact f = \input -> ReacT $ return $ Right (f input, preact f)
-                           
-                
-                                
-                                                                      
-
