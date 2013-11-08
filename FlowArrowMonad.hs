5816b58b5dc0824b7609917d5e3b70a034259f94
Ian Graves <thegravian@gmail.com>
2013-11-08 14:15:53 -0600

    Moved over to FlowArrowMonad

--- FlowArrowMonad.hs
+++ FlowArrowMonad.hs
@@ -1,8 +1,12 @@
-module FlowArrowMonad where
+{-# LANGUAGE RecursiveDo #-}
 
+module FlowArrowMonad where
 import Control.Category
 import Control.Arrow
 import Prelude hiding (id,(.))
+import Control.Monad.Fix
+import Control.Monad.Identity
+import Debug.Trace
 
 newtype Flow m i o = Flow { deFlow :: (i -> m (Maybe o,Flow m i o)) }
 
@@ -11,15 +15,29 @@ instance Monad m => Category (Flow m) where
   a . b = (flip (<//>)) a b
 
 (<//>) :: Monad m => Flow m a b -> Flow m b c -> Flow m a c
-Flow k1 <//> Flow k2 = Flow (\ i -> do (mn,k1') <- k1 i
-                                       case mn of
-                                         Just n -> do (o,k2') <- k2 n
-                                                      return (o,k1' <//> k2')
-                                         Nothing -> return (Nothing,k1' <//> Flow k2))
+f@(Flow k1) <//> g@(Flow k2) = Flow (\ i -> do (mn,f') <- k1 i
+                                               case mn of
+                                                 Just n -> do (o,g') <- k2 n
+                                                              return (o,f' <//> g')
+                                                 Nothing -> return (Nothing,f' <//> g))
 
 instance Monad m => Arrow (Flow m) where
   arr f = Flow (\ i -> return (Just (f i),arr f))
-  first (Flow k) = Flow (\ (i,x) -> do (mo,k') <- k i
+  first (Flow k) = Flow (\ (i,x) -> do (mo,f') <- k i
                                        case mo of
-                                         Just o  -> return (Just (o,x),first k')
-                                         Nothing -> return (Nothing,first k'))
+                                         Just o  -> return (Just (o,x),first f')
+                                         Nothing -> return (Nothing,first f'))
+
+instance Monad m => ArrowChoice (Flow m) where
+  left f@(Flow k) = Flow (\ ei -> case ei of
+                                    Left i  -> do (mn,f') <- k i
+                                                  case mn of
+                                                    Just n  -> return (Just (Left n),left f')
+                                                    Nothing -> return (Nothing,left f')
+                                    Right i -> return (Just (Right i),left f))
+
+instance MonadFix m => ArrowLoop (Flow m) where
+  loop (Flow k) = Flow (\ b -> mdo (mr@(~(Just (c,d))),f') <- k (b,d)
+                                   case mr of
+                                     Just _  -> return (Just c,loop f')
+                                     Nothing -> return (Nothing,loop f'))
