5dd26142aaad51f4d8ea8f1abc36d9a0fa837b06
Ian Graves <thegravian@gmail.com>
2013-11-09 10:34:38 -0600

    Some writing.  This can stay or go later.

--- FlowArrowMonad.hs
+++ FlowArrowMonad.hs
@@ -9,14 +9,32 @@ import Control.Monad.Fix
 import Control.Monad.Identity
 import Control.Monad.Trans
 import Control.Applicative
-import Debug.Trace
 
+{- | The Flow type for sequencing monadic computations.  
+ -   Underlying monad m, input type i, and output type o.  
+ -   Flow is a function that takes the input i and returns
+ -   a pair of Maybe output and a (potentially updated) version
+ -   of itself to call against again in the future.  The output is
+ -   Maybe o instead of o to reflect that some calls against a Flow
+ -   may not always readily return a value.  In situations like this,
+ -   one should take the updated flow and call it again to try for a
+ -   result in a future call.  This feature enables the modeling of 
+ -   things like stalls or a "currently empty buffer" in a stream.  The 
+ -   instances of Flow in the various typeclasses below assume this 
+ -   behavior and implement it.
+ -}
 newtype Flow m i o = Flow { deFlow :: (i -> m (Maybe o,Flow m i o)) }
 
+-- Instance of Category for Arrow support
 instance Monad m => Category (Flow m) where
   id = Flow (\ i -> return (Just i,id))
   a . b = (flip (<//>)) a b
 
+{- | Flow sequence operator.  Takes two flows and chains them left to right.
+ -   If the left hand side yields a nothing output, the sequence operator is
+ -   executed against the updated version of the left hand argument to try
+ -   again for a non-nothing output.
+ -}
 (<//>) :: Monad m => Flow m a b -> Flow m b c -> Flow m a c
 f@(Flow k1) <//> g@(Flow k2) = Flow (\ i -> do (mn,f') <- k1 i
                                                case mn of
@@ -24,6 +42,8 @@ f@(Flow k1) <//> g@(Flow k2) = Flow (\ i -> do (mn,f') <- k1 i
                                                               return (o,f' <//> g')
                                                  Nothing -> return (Nothing,f' <//> g))
 
+-- (Flow m) is an instance of Arrow.  (Flow m) i o is analogous to 
+-- the Arrow a => a b c
 instance Monad m => Arrow (Flow m) where
   arr f = Flow (\ i -> return (Just (f i),arr f))
   first (Flow k) = Flow (\ (i,x) -> do (mo,f') <- k i
@@ -31,7 +51,7 @@ instance Monad m => Arrow (Flow m) where
                                          Just o  -> return (Just (o,x),first f')
                                          Nothing -> return (Nothing,first f'))
 
-
+-- Instance of ArrowChoice which gives choice operators for Arrows.
 instance Monad m => ArrowChoice (Flow m) where
   left f@(Flow k) = Flow (\ ei -> case ei of
                                     Left i  -> do (mn,f') <- k i
@@ -40,6 +60,8 @@ instance Monad m => ArrowChoice (Flow m) where
                                                     Nothing -> return (Nothing,left f')
                                     Right i -> return (Just (Right i),left f))
 
+-- ArrowLoop is for computations that that feed back results into themselves
+-- This was defined by Adam's genius for Flows over MonadFix.
 instance MonadFix m => ArrowLoop (Flow m) where
   loop (Flow k) = Flow (\ b -> mdo (mr@(~(Just (c,d))),f') <- k (b,d)
                                    case mr of
@@ -47,40 +69,60 @@ instance MonadFix m => ArrowLoop (Flow m) where
                                      Nothing -> return (Nothing,loop f'))
 
 
+--If the underlying monad is MonadPlus, this feature rises to the Arrow level.
 instance MonadPlus m => ArrowZero (Flow m) where
   zeroArrow = Flow (\ _ -> mzero)
-
 
 instance MonadPlus m => ArrowPlus (Flow m) where
   (Flow k1) <+> (Flow k2) = Flow $ \ i -> do (k1 i `mplus` k2 i)
 
+
+--The "chaining" model is also a Functor.  fmap is equivalent to lifting a pure
+--function into Flow and "appending it to the end of a computation" using the
+--arr function to lift pure functions into an Arrow.
 instance Monad m => Functor (Flow m i) where
   fmap f k1 = k1 <//> arr f
 
+
+{-Is an applicative functor as well.  "Pure" is taking a value and forming a Flow
+ - that returns a pair with the value as the output and a resuming computation that 
+ - recursively does the same thing.  
+ -
+ - <*> :: f (a -> b) -> f a -> f b in Flow forms a Flow that runs the left hand side 
+ - until it yields a function (a -> b) and then tries to run the right hand side until 
+ - it gets an (a) and then applies the function for the result.
+ -}
 instance Monad m => Applicative (Flow m i) where
   pure a  = Flow (\ _ -> return (Just a, pure a)) 
-  (Flow f) <*> (Flow g) = Flow $ \ i -> do f' <- f i
-                                           case f' of
-                                                (Nothing,fres) -> return (Nothing,fres <*> (Flow g))
-                                                (Just fo,fres) -> do 
-                                                                   g' <- g i
-                                                                   case g' of
-                                                                        (Nothing,gres) -> return (Nothing,(Flow f) <*> gres)
-                                                                        (Just go,gres) -> return (Just (fo go), fres <*> gres)
+  (Flow f) <*> (Flow g) = Flow (\ i -> do (mf, f') <- f i
+                                          case mf of
+                                               Nothing -> return (Nothing,f' <*> (Flow g))
+                                               Just fo -> do 
+                                                                  (mg,g') <- g i
+                                                                  case mg of
+                                                                       Nothing -> return (Nothing,(Flow f) <*> g')
+                                                                       Just go -> return (Just (fo go), f' <*> g'))
                                                                    
                                            
                                                  
+--You can treat Flow as a Monad.  Binding is a little strange, though.  The input
+--given to run the right is also fed as the input to run the left after the left is 
+--computed from the output given by running the right.  Not sure if this follows the 
+--Monad laws.
 instance Monad m => Monad (Flow m i) where                                                
   return o = Flow (\ _ -> return (Just o, return o))
   Flow k1 >>= g = Flow (\ i -> do (mn,f') <- k1 i
                                   case mn of
                                     Nothing -> return (Nothing,f' >>= g)
                                     Just n  -> deFlow (g n) i)
-  
+
+--Flow is also a monad transformer.  
 liftFlow :: Monad m => m o -> Flow m i o
 liftFlow m = Flow (\ _ -> do o <- m
                              return (Just o,liftFlow m))
 
+-- The underneath is a redefining of Flow so it can be fit into the MonadTrans type
+-- as it's laid out in the MTL.
 newtype FlowT i m o = FlowT { deFlowT :: Flow m i o }
 
 instance MonadTrans (FlowT i) where
